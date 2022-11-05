7a1bda925e978e9e7d90ff159dfe5877dd066d6a
Okirey <111231755+Okirey@users.noreply.github.com>
2022-11-05 15:00:01 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     
+     <div className="container">
         Weather App
-        
+      
+        </div>
     </div>
   );
 }
 
-export default App;
+
