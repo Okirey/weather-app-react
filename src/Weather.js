fc8683d0334155291220d1c2f4ee79dafcfc9532
Okirey <111231755+Okirey@users.noreply.github.com>
2022-11-06 16:08:08 +0300

    Added an api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function Weather(){
-    return (
+export default function Weather(props){
+
+const [weatherData, setWeatherData] = useState({ready:false});
+    function handleResponse(response){
+    console.log(response.data);
+    setWeatherData(
+        {   ready:true,
+            temperature:response.data.temperature.current,
+            city: response.data.city,             
+            wind: Math.round(response.data.wind.speed),
+            humidity: response.data.temperature.humidity,
+            description: response.data.condition.description,
+            dayAndTime: "Saturday 15:30"
+        }
+    )
+ 
+}
+if(weatherData.ready){
+ return (
  <div className="Weather">
     <form>
         <div className="row">
@@ -15,10 +33,10 @@ export default function Weather(){
         </div>
         </div>
     </form>
-    <h1>Malaga</h1>
+    <h1>{weatherData.city}</h1>
     <ul>
-        <li>Saturday 15:13</li>
-        <li>Clear</li>
+        <li>{weatherData.dayAndTime}</li>
+        <li className ="text-capitalize">{weatherData.description}</li>
     </ul>
     <div className="row mt-1">
         <div className="iconAndTemp col-6">
@@ -29,16 +47,23 @@ export default function Weather(){
         animate={true}
        
       />
-      <span className="temperature">  25</span>
+      <span className="temperature">  {Math.round(weatherData.temperature)}</span>
       <span className="unit">°C</span> 
         </div>
         <div className="col-6">
             <ul className="mt-3">
-                <li>Humidity: 80%</li>
-                <li>Wind: 5 km/h</li>
+                <li>Humidity: {weatherData.humidity} %</li>
+                <li>Wind: {weatherData.wind} km/h</li>
             </ul>
         </div>        
     </div>
  </div>
     )
+} else{
+    const apiKey = `fbfc1ff848ef3c808f7at85bodf0ef27`;
+    let city = `malaga`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading";
+}   
 }
